d0efebefd3fa2714465160e1122e523152078e6d
Jahidul-islam-rakib <168633979+Jahidul-islam-rakib@users.noreply.github.com>
2024-09-10 13:40:32 +0600

    Add files via upload

---
+++ LibraryManagement/LibraryManagement/DB/LibraryDbContext.cs
@@ -0,0 +1,15 @@
+﻿using LibraryManagement.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryManagement.DB
+{
+    public class LibraryDbContext : DbContext
+    {
+        public LibraryDbContext(DbContextOptions<LibraryDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Library>Librarys{get; set;}
+    }
+}
---
+++ LibraryManagement/LibraryManagement/Migrations/20240909173853_AddLibrary.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LibraryManagement.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLibrary : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Librarys",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Librarys", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Librarys");
+        }
+    }
+}
